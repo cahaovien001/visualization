f1eaa92bf5aa8c47cd738a4c0d1d56ea3dafcba6
Cahao L. Vien <cahao@cahao-s-debian-pc.vien>
2018-08-08 00:28:51 -0400

    added my personal homepage

---
+++ VisualizeToRedSpectrum.java
@@ -0,0 +1,149 @@
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.awt.image.BufferedImage;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+class ImageDrawingComponent extends JComponent {
+    private int width;
+    private int height;
+    private BufferedImage bImg;
+    private Graphics g;
+    
+    public ImageDrawingComponent(String filename) {
+	try {
+	    BufferedImage bImg = ImageIO.read(new File(filename));
+	    int width  = bImg.getWidth (null);
+	    int height = bImg.getHeight(null);
+
+	    BufferedImage bImgRGB =
+		new BufferedImage(width,
+				  height,
+				  BufferedImage.TYPE_INT_RGB);
+	    
+	    Graphics g = bImgRGB.getGraphics();
+	    g.drawImage(bImg, 0, 0, null);
+	    bImg = bImgRGB;
+
+	    this.width = width;
+	    this.height = height;
+	    this.bImg = bImg;
+	    this.g = g;
+	} catch(Exception e) {
+	    System.err.println(":(");
+	}
+    }
+
+    public Dimension getPreferredSize() {
+	return new Dimension(width, height);
+    }
+
+    public void paint(Graphics g) {
+	Graphics2D gg = (Graphics2D) g;
+
+	int minX = bImg.getMinX();
+	int minY = bImg.getMinY();
+	int maxX = bImg.getWidth();
+	int maxY = bImg.getHeight();
+
+	int widthScale =  (int)((double)(maxX - minX) /233.);
+	int heightScale = (int)((double)(maxY - minY) /233.); 
+	
+	for (int i = minY; i<maxY; i++) {
+	    for (int j = minX; j<maxX; j++) {
+		if (j % widthScale == 0 &&
+		    i % heightScale == 0) {
+		    Color color = new Color(bImg.getRGB(j, i));
+		    int red   = color.getRed();
+		    int green = color.getGreen();
+		    int blue  = color.getBlue();
+		    int letter;
+
+		    if (green > 64 || blue > 64) {
+			gg.setColor(new Color(0,
+					      green,
+					      0));
+			
+			gg.drawOval(j, i, 3, 3);
+			gg.fillOval(j, i, 3, 3);
+		    }
+		}
+	    }
+	}
+
+	
+	// widthScale =  (int)((double)(maxX - minX) /89.);
+	// heightScale = (int)((double)(maxY - minY) /89.);
+	    
+	// for (int i = minY; i<maxY; i++) {
+	//     for (int j = minX; j<maxX; j++) {
+	// 	if (j % widthScale == 0 &&
+	// 	    i % heightScale == 0) {
+	// 	    Color color = new Color(bImg.getRGB(j, i));
+	// 	    int red   = color.getRed();
+	// 	    int green = color.getGreen();
+	// 	    int blue  = color.getBlue();
+	// 	    int letter;
+
+	// 	    gg.setColor(new Color((int) (0.236f*(float)red),
+	// 				  green,
+	// 				  0));
+	// 	    gg.drawRect(j, i, 1, 1);
+	// 	    gg.fillRect(j, i, 1, 1);
+		
+	// 	}
+	//     }
+	// }
+
+	// widthScale =  (int)((double)(maxX - minX) /21.);
+	// heightScale = (int)((double)(maxY - minY) /21.);
+	
+	// for (int i = minY; i<maxY; i++) {
+	//     for (int j = minX; j<maxX; j++) {
+	// 	if (j % widthScale == 0 &&
+	// 	    i % heightScale == 0) {
+	// 	    Color color = new Color(bImg.getRGB(j, i));
+	// 	    int red   = color.getRed();
+	// 	    int green = color.getGreen();
+	// 	    int blue  = color.getBlue();
+	// 	    int letter;
+
+	// 	    if (red > 128) {
+	// 		gg.setColor(new Color((int) (0.618f *(float) red),
+	// 				      green,
+	// 				      (int) (0.236f *(float) blue)));
+	// 		gg.drawRect(j, i, 1, 1);
+	// 		gg.fillRect(j, i, 1, 1);
+	// 	    }
+	// 	}
+	//     }
+	//}
+    }
+}
+	
+
+public class VisualizeToRedSpectrum {
+    
+    
+    public static void main(String... args) {
+	try {
+	    String filename = args[0];
+
+	    JFrame frame = new JFrame("ImageGradient");
+	    ImageDrawingComponent idc = new ImageDrawingComponent(filename);
+
+	    frame.add("Center", idc);
+	    frame.pack();
+	    frame.setVisible(true);
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	} catch(Exception e) {
+	    System.err.println(":(");
+
+	}
+    }
+}
